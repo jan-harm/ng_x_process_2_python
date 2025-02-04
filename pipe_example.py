5164ed5d77af526d03ca17265cab962277f07a3e
jan-harm <jan.harm@deboerenwolters.nl>
2025-02-04 16:39:45 +0100

    update added delay example

--- pipe_example.py
+++ pipe_example.py
@@ -4,18 +4,46 @@
 import ng_x_py as ng
 import logging
 import logging_tree
+from bitarray.util import zeros
 
-# todo: register available functions
-#  register
+class Delay:
+    """Simple delay function
+    with one input shift input in delay line
+    with two inputs, first is data second is enable
+    one output bit active (more are ignored)
+    Note minimal delay is one.
+    """
+
+    def __init__(self, nr_input_bits, nr_output_bits, par1=2):
+        if nr_input_bits == 0:
+            logger.warning(' delay should have at least one bit input for data')
+            raise Exception('not enough inputs for delay')
+        self.clocks_delay = par1 if par1 > 1 else 1
+        self.enable = nr_input_bits > 1
+        self.delay_register = zeros(self.clocks_delay)
+        self.nr_output_bits = nr_output_bits
+        logger.debug(f'init delay, total delay is %d clocks, enable is %s', self.clocks_delay, str(self.enable))
+
+    def update(self, input_bits):
+        """ update shift register when enabled"""
+        enabled = True
+        if self.enable:
+            enabled = input_bits[1] == 1
+        if enabled:
+            self.delay_register >>= 1
+            self.delay_register[0] = input_bits[0]
+        ret_val = self.delay_register[-1:]
+        return ret_val
+
+
+
+ng.register_function('delay', Delay)
+
 logger = logging.getLogger(__name__)
 logger.setLevel('DEBUG')
 
-app = ng.App(logger)
+app = ng.App(logger) # initialize all
 
-with open('logtree.tmp', 'w') as file_object:
-    file_object.write(logging_tree.format.build_description())
-    file_object.close()
-# lg.setLevel(ng.logging.DEBUG)
 logger.info('----------------------starting loop  log from main')
 app.run()
 logger.info('-------------------------ending loop  log from main')
